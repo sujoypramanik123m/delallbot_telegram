e8667462c0178f329a440b32f2e62c996fdf8787
sujoypramanik123m <sujoypramanik123m@gmail.com>
2025-05-26 12:36:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 # Configuration file for the Telegram bot
 
-API_ID = 'YOUR_API_ID'
-API_HASH = 'YOUR_API_HASH'
-BOT_TOKEN = 'YOUR_BOT_TOKEN'
+API_ID = '22182189'
+API_HASH = '5e7c4088f8e23d0ab61e29ae11960bf5'
+BOT_TOKEN = '7994699443:AAH1CV-Pr1vN86pOO1JFqve0ZJQ8h_At-eo'
 
 # MongoDB configuration
-DB_URL = 'mongodb://localhost:27017/'  # Replace with your MongoDB URI
+DB_URL = 'mongodb+srv://sujoy123m:wTWKGUaxYE7dxb1l@cluster0.zorxb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'  # Replace with your MongoDB URI
 DB_NAME = 'telegram_bot'
 
 # Log channel ID where notifications about new users will be sent
-LOG_CHANNEL = -1001234567890  # Replace with your log channel ID
+LOG_CHANNEL = -1002634001741  # Replace with your log channel ID
