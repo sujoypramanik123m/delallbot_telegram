f3a9cd3efb6d3a3217343ec54d2b2884157eae6c
sujoypramanik123m <sujoypramanik123m@gmail.com>
2025-05-26 14:51:03 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
-import os
+# config.py
 
-API_ID = os.getenv('API_ID')
-API_HASH = os.getenv('API_HASH')
-BOT_TOKEN = os.getenv('BOT_TOKEN')
-DB_URL = os.getenv('DB_URL')
-DB_NAME = os.getenv('DB_NAME')
-LOG_CHANNEL_ID = os.getenv('LOG_CHANNEL_ID')
-OWNER_ID = os.getenv('OWNER_ID')
+API_ID = '22182189'
+API_HASH = '5e7c4088f8e23d0ab61e29ae11960bf5'
+BOT_TOKEN = '7994699443:AAH1CV-Pr1vN86pOO1JFqve0ZJQ8h_At-eo'
+DB_URL = 'mongodb+srv://sujoy123m:wTWKGUaxYE7dxb1l@cluster0.zorxb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+DB_NAME = 'delall_log'
+LOG_CHANNEL_ID = '-1002634001741'
+OWNER_ID = '8181241262'
